9b4790838a4fff99504b6e4f9d19756290b85a5a
Raphael Haehnel <raphael.haehnel@gmail.com>
2023-05-11 22:07:29 +0300

    Itay it's our turn

--- primeFieldElement.py
+++ primeFieldElement.py
@@ -133,5 +133,19 @@ class PrimeFieldElement:
         error = f"Cannot find inverse for {self.a} in PrimeField {self.p}"
         raise TypeError(error)
 
+    def gcdExtended(self, a, b):
+        # Base Case
+        if a == 0:
+            return b, 0, 1
+
+        gcd, x1, y1 = self.gcdExtended(b % a, a)
+
+        # Update x and y using results of recursive
+        # call
+        x = y1 - (b//a) * x1
+        y = x1
+
+        return gcd, x, y
+
     def __repr__(self):
         return str(self.a)
